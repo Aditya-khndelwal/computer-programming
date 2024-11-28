bc7a0bbf0cebe38f9ed4777fd3b0c1e857b7c460
Aditya-khndelwal <adityakhandelwalg@gmail.com>
2024-11-28 19:06:54 +0530

    Create function_q19.py

---
+++ coding-assignment/function/function_q19.py
@@ -0,0 +1,6 @@
+#Q19 = Write a function palindrome that checks if a given string is a palindrome and print the reversed string as well.
+def palindrome(s):
+    print("reversed string is:",s[::-1])
+    return s == s[::-1] 
+s = input("enter string:")
+print(palindrome(s))
