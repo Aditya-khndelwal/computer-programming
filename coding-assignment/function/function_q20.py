88b8473121b7718934f55b8823f4af25d8baff92
Aditya-khndelwal <adityakhandelwalg@gmail.com>
2024-11-28 19:07:26 +0530

    Create function_q20.py

---
+++ coding-assignment/function/function_q20.py
@@ -0,0 +1,9 @@
+#Q20 = Define a function gcd that calculates the greatest common divisor of two numbers using recursion.
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+print("greatest common divisor is:",gcd(a,b))
