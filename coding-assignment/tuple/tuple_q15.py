3f1708cb6cbb9497ff53f82ad23184f63928f442
Aditya-khndelwal <adityakhandelwalg@gmail.com>
2024-11-28 19:34:03 +0530

    Create tuple_q15.py

---
+++ coding-assignment/tuple/tuple_q15.py
@@ -0,0 +1,4 @@
+#Given a tuple of strings, concatenate all the strings into one single string and print it:
+t = ("Hello", "world", "this", "is", "Python")
+concatenated_string = ''.join(t)
+print(concatenated_string)
