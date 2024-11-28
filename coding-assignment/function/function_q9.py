f1f19140515a0068f5e5e5b31e1a089f47d6a225
Aditya-khndelwal <adityakhandelwalg@gmail.com>
2024-11-28 18:57:47 +0530

    Create function_q9.py

---
+++ coding-assignment/function/function_q9.py
@@ -0,0 +1,5 @@
+#Q9 = Define a function sum_list that takes a list of numbers and returns their sum.
+def sum_list(numbers):
+    print(sum(numbers))
+numbers = list(map(int,input().split()))
+sum_list(numbers)
