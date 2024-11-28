256087b140377574792e87c9d711565e7563168a
Aditya-khndelwal <adityakhandelwalg@gmail.com>
2024-11-28 18:52:12 +0530

    Create function_q4.py

---
+++ coding-assignment/function/function_q4.py
@@ -0,0 +1,8 @@
+#Q4 = Define a function is_even that takes a number and returns True if it's even, otherwise False.
+def is_even(num):
+    if num % 2 == 0:
+        print(True)
+    else:
+        print(False)
+num = int(input("enter the number:"))
+is_even(num)
